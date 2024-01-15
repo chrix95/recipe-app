a0fc974eecced76d0336405a88d73ad09f4af79b
Okokon Christopher Ntuk <engchris95@gmail.com>
2024-01-15 01:08:13 +0000

    Added HTTP header status

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,23 +2,22 @@ class RecipesController < ApplicationController
     before_action :set_category_params, :set_recipe_id_params, only: [:filter, :lookup]
 
     def index
-        categories = RecipeService.fetch_categories
         render json: { status: 200, message: "Welcome to Recipes API v1" }
     end
 
     def categories
         categories = RecipeService.fetch_categories
-        render json: JsonTransformService.transform_keys_to_snake_case(categories)
+        render json: JsonTransformService.transform_keys_to_snake_case(categories), status: categories[:status]
     end
 
     def filter
         recipes = RecipeService.filter_category(@category)
-        render json: JsonTransformService.transform_keys_to_snake_case(recipes)
+        render json: JsonTransformService.transform_keys_to_snake_case(recipes), status: recipes[:status]
     end
 
     def lookup
         recipe = RecipeService.lookup_recipe(@id)
-        render json: JsonTransformService.transform_keys_to_snake_case(recipe)
+        render json: JsonTransformService.transform_keys_to_snake_case(recipe), status: recipe[:status]
     end
 
     private
